212180a0b01136351a9d9ea44b5b5185236d266d
Nick Pafundi <nick@cloversites.com>
2014-11-19 21:38:12 -0800

    Quoted paths don't work properly in [[ -f path ]] test

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,10 +35,10 @@ read give_links
 [[ "$give_links" == 'y' ]] && open_apps
 
 echo 'Checking for SSH key, generating one if it does not exist...'
-  [[ -f '~/.ssh/id_rsa.pub' ]] || ssh-keygen -t rsa
+  [[ -f ~/.ssh/id_rsa.pub ]] || ssh-keygen -t rsa
 
 echo 'Copying public key to clipboard. Paste it into your Github account...'
-  [[ -f '~/.ssh/id_rsa.pub' ]] && cat '~/.ssh/id_rsa.pub' | pbcopy
+  [[ -f ~/.ssh/id_rsa.pub ]] && cat ~/.ssh/id_rsa.pub | pbcopy
   open 'https://github.com/account/ssh'
 
 
